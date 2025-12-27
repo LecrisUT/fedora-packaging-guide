586c166b08ec058f1f4a04373748e07cf80601bf
Cristian Le <git@lecris.dev>
2025-12-28 01:06:58 +0700

    Add the how_to_read contents

--- docs/conf.py
+++ docs/conf.py
@@ -9,10 +9,12 @@ extensions = [
     "sphinx_design",
     "sphinx.ext.todo",
     "sphinx_subfigure",
+    "sphinx_togglebutton",
 ]
 
 html_theme = "furo"
 
 myst_enable_extensions = [
     "colon_fence",
+    "deflist",
 ]
